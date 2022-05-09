a102cc0590a5ab516450cd47bf2436905d99b13c
Yaroslav Khapalov <y.hapalov@gmail.com>
2022-05-09 19:11:10 +0300

    fix: webpack config update

--- keaboard/webpack.config.js
+++ keaboard/webpack.config.js
@@ -1,20 +1,19 @@
-const path = require("path");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  mode: "development",
   entry: {
-    main: path.resolve(__dirname, "./src/index.js"),
+    main: path.resolve(__dirname, './src/index.js'),
   },
 
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "main.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
   },
 
   devServer: {
     static: {
-      directory: path.join(__dirname, "./dist"),
+      directory: path.join(__dirname, './dist'),
     },
     open: true,
     hot: true,
@@ -24,7 +23,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
